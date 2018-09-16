14743a6adde2af0bba70719b281f8927052fdd22
sjayasinghe1 <sjayasinghe1@kpmg.com>
2018-09-16 12:49:15 +0530

    sales return summary store sale

---
+++ LSL/Integration/JE Import/returns-summary.sql
@@ -0,0 +1,87 @@
+CREATE OR REPLACE PROCEDURE create_returns_summary (p_data  IN  CLOB)
+AS
+
+  l_data           varchar2(20000);
+  l_buffer         varchar2(32767);
+  l_amount         number;
+  l_offset         number;
+
+BEGIN
+    l_data := p_data;
+    --l_data := '[{"date":"2018-06-04T00:00:00","products":[{"saleSummaryID":1,"date":"2018-06-04T00:00:00","productId":"d06bd7ed-6fe1-4a36-9050-d49d36966b71","partNumber":"000001","title":"Product One","avgCostEx":4.5455,"avgCostTax":0.4546,"unit":"Each","totalUnits":1.000,"totalValueEx":18.1800,"totalValueTax":1.8200}]}]';
+
+   INSERT INTO returns_summary_tbl (
+                                    sales_date      ,
+                                    salesummaryid  
+                                  )
+                        SELECT *
+                        FROM 
+                             JSON_TABLE(
+                             l_data
+                             , '$[*]' COLUMNS (
+                              "sales_date" varchar2(30) PATH '$.date',
+                                      NESTED PATH '$.products[*]' COLUMNS (
+                                                   salesummaryid   number      PATH '$.saleSummaryID'
+                                      )
+                        
+                              )
+                        ) JT;
+                        
+               INSERT INTO salessummaryproduct (
+                                        sales_date      ,
+                                        salesummaryid   ,
+                                        sale_date       ,
+                                        productId       ,
+                                        partNumber      ,
+                                        title           ,
+                                        avgCostEx       ,
+                                        avgCostTax      ,
+                                        unit            ,
+                                        totalUnits      ,
+                                        totalValueEx    ,
+                                        totalValueTax
+                                      )
+                                SELECT *
+        FROM 
+             JSON_TABLE(
+             l_data
+             , '$[*]' COLUMNS (
+              "sales_date" varchar2(30) PATH '$.date',
+                      NESTED PATH '$.products[*]' COLUMNS (
+                                   
+                                   salesummaryid   number      PATH '$.saleSummaryID',
+                                                  sale_date       varchar2(30)        PATH '$.date',
+                                                  productId       varchar2(300)    PATH '$.productId',
+                                                  partNumber      varchar2(300)    PATH '$.partNumber',
+                                                  title           varchar2(300)    PATH '$.title',
+                                                  avgCostEx       number      PATH '$.avgCostEx',
+                                                  avgCostTax      number      PATH '$.avgCostTax',
+                                                  unit            varchar(300)     PATH '$.unit',
+                                                  totalUnits      number      PATH '$.totalUnits',
+                                                  totalValueEx    number      PATH '$.totalValueEx',
+                                                  totalValueTax   number      PATH '$.totalValueTax'
+                                   
+        )
+        
+        )) JT;                       
+        
+            COMMIT;
+--
+--  l_amount := 32000;
+--  l_offset := 1;
+--  
+--        begin
+--            loop
+--                dbms_lob.read( l_clob, l_amount, l_offset, l_buffer );
+--                htp.p(l_buffer);
+--                l_offset := l_offset + l_amount;
+--                l_amount := 32000;
+--            end loop;
+--        exception
+--            when no_data_found then
+--                DBMS_OUTPUT.put_line('Message: unsuccessful operation');c v
+--            when others then
+--                DBMS_OUTPUT.put_line(SQLERRM);
+--        end;
+
+END;
--- LSL/Integration/JE Import/store.sql
+++ LSL/Integration/JE Import/store.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE PROCEDURE create_sale (p_data  IN  CLOB)
+CREATE OR REPLACE PROCEDURE create_store (p_data  IN  CLOB)
 AS
 
   l_data           varchar2(20000);
@@ -10,36 +10,20 @@ BEGIN
     l_data := p_data;
     --l_data := '[{"date":"2018-06-04T00:00:00","products":[{"saleSummaryID":1,"date":"2018-06-04T00:00:00","productId":"d06bd7ed-6fe1-4a36-9050-d49d36966b71","partNumber":"000001","title":"Product One","avgCostEx":4.5455,"avgCostTax":0.4546,"unit":"Each","totalUnits":1.000,"totalValueEx":18.1800,"totalValueTax":1.8200}]}]';
 
-   INSERT INTO sale (
-                      SaleId
+   INSERT INTO "store" (
+                      StoreID,
+                      Title
                     )
                         SELECT *
                         FROM 
                              JSON_TABLE(
                              l_data
                              , '$[*]' COLUMNS (
-                                                SaleId varchar2(30) PATH '$.SaleId'
+                                                StoreID varchar2(30) PATH '$.StoreID'
+                                                Title   varchar2(100) PATH '$.Title'
                                               )
                            ) JT;
-   
-   
-   INSERT INTO saledetail (
-                              SaleId,
-                              SaleDetailId
-                            )
-                        SELECT *
-                        FROM 
-                             JSON_TABLE(
-                             l_data
-                             , '$[*]' COLUMNS (
-                              SaleId varchar2(30) PATH '$.SaleId',
-                                      NESTED PATH '$.Lines[*]' COLUMNS (
-                                                   
-                                                                  SaleDetailId   number      PATH '$.SaleDetailId'
-                                                   
-                                      )
-                              )
-                        ) JT;           
+            
             COMMIT;
 --
 --  l_amount := 32000;
