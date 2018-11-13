97918da565763dcdbf2a4ad1b0f75cb7f4fded01
Sayuru Jayasinghe <sjayasinghe1@kpmg.com>
2018-11-13 18:18:08 +0530

    sh for .cer modified

--- LSL/Regression Testing/testsh.sh
+++ LSL/Regression Testing/testsh.sh
@@ -1,5 +1,11 @@
+echo "Download SSL .cer for PBSA stores"
+
+var=0
+while IFS= read -r line; do
+	var1=$((var++))
+	echo "" | openssl s_client -host ${line} -port 31058 | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p'  > LSL${var}.cer
+
 echo "Create Oracle wallet for PBSA stores"
-var=2
 path="/home/oraapex/wallet${var}"
 cer_path=${path}/LSL${var}.cer
 
@@ -7,8 +13,10 @@ echo "Create the wallet folder"
 mkdir -p ${path}
 
 echo "Copy the certification file .cer"
-cp /tmp/LSL${var}.cer ${path}
+cp /home/oraapex/LSL${var}.cer ${path}
 
 echo "Create the wallet"
 orapki wallet create -wallet ${path} -pwd Pa$$w0rd -auto_login
 orapki wallet add -wallet ${path} -trusted_cert -cert ${cer_path} -pwd Pa$$w0rd
+
+done < ip-file.txt
